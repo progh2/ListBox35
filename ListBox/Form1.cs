9a5be45ff7aaa2f1a392d51cd6b80d6c82f22cf0
Gihun Ham <progh2@gmail.com>
2019-06-03 09:05:28 +0900

    #2 ListBox에 항목을 추가하고, 선택하는 코드 구현

--- ListBox/Form1.cs
+++ ListBox/Form1.cs
@@ -11,9 +11,49 @@ namespace ListBox
 {
     public partial class Form1 : Form
     {
+        private string OrgStr = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblResult.Text;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            CheckText();
+        }
+
+        private void CheckText()
+        {
+            if (this.txtList.Text != "")
+            {
+                this.lbView.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("항목을 입력하세요", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            this.txtList.Focus();
+        }
+
+        private void TxtList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                CheckText();
+                e.Handled = true;
+            }
+        }
+
+        private void LbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.lbView.Text;
         }
     }
 }
